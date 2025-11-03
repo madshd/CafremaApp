f07b0490ee515106eeb6642a5d86c1d97ac1d240
Mads D <42411664+madshd@users.noreply.github.com>
2025-11-03 09:58:53 +0100

    Initial commit

---
+++ CafremaApp.Core/Entities/Tenant.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CafremaApp.Domain.Entities
+{
+    internal class Tenant
+    {
+    }
+}
---
+++ CafremaApp.Infrastructure/Data/Context.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CafremaApp.Infrastructure.Data;
+
+public class Context : DbContext
+{
+    public Context(DbContextOptions options) : base(options) { }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        // // Supabase-tabeller ligger ofte i 'public' skemaet
+        // modelBuilder.HasDefaultSchema("public");
+    }
+}
