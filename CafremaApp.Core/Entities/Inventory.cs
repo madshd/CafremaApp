adf2eb603579620ac73e9851a5721c2ac080c698
eaa23jnk <eaa23jnk@students.eaaa.dk>
2025-11-03 11:56:45 +0100

    Inventory now has Id

--- CafremaApp.Core/Entities/Inventory.cs
+++ CafremaApp.Core/Entities/Inventory.cs
@@ -4,13 +4,15 @@ namespace CafremaApp.Domain.Entities;
 
 public class Inventory
 {
+    private string Id { get; set; }
     private string Type { get; set; }
     private DateOnly InstallationDate { get; set; }
     private Condition Condition { get; set; }
     private string Comment { get; set; }
 
-    public Inventory(string type, DateOnly installationDate, Condition condition, string comment)
+    public Inventory(string id, string type, DateOnly installationDate, Condition condition, string comment)
     {
+        Id = id;
         Type = type;
         InstallationDate = installationDate;
         Condition = condition;
@@ -23,8 +25,8 @@ public class Appliance : Inventory
     private string Manufacturer { get; set; }
     private string Model { get; set; }
 
-    public Appliance(string type, DateOnly installationDate, Condition condition, string comment, 
-        string manufacturer, string model) : base(type, installationDate, condition, comment)
+    public Appliance(string id, string type, DateOnly installationDate, Condition condition, string comment, 
+        string manufacturer, string model) : base(id, type, installationDate, condition, comment)
     {
         Manufacturer = manufacturer;
         Model = model;
