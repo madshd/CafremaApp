c433eb547705b0594a71dad3c5d65757a17d5eae
Mads D <42411664+madshd@users.noreply.github.com>
2025-11-03 12:05:27 +0100

    Metoder til hentning og CRUD på Inventory

---
+++ CafremaApp.Core/Interfaces/IInventoryRepository.cs
@@ -0,0 +1,9 @@
+﻿namespace CafremaApp.Infrastructure.Repositories;
+
+public interface IInventoryRepository
+{
+    Task<Inventory?> GetByIdAsync(Guid id);
+    Task AddAsync(Inventory inventory);
+    Task UpdateAsync(Inventory inventory);
+    Task DeleteAsync(Guid id);
+}
---
+++ CafremaApp.Infrastructure/Repositories/InventoryRepository.cs
@@ -0,0 +1,24 @@
+﻿namespace CafremaApp.Infrastructure.Repositories;
+
+public class InventoryRepository :  IInventoryRepository
+{
+    public Task<Inventory?> GetByIdAsync(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task AddAsync(Inventory inventory)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task UpdateAsync(Inventory inventory)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task DeleteAsync(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+}
