005b0055074dc73c6a475ced42f4766368dc69aa
Rasmus Meinert Folke Flebbe <rasmusmeinertfolkeflebbe@gmail.com>
2025-11-10 11:03:22 +0100

    DTO'er

---
+++ CafremaApp.Application/DTOs/InventoryDTO.cs
@@ -0,0 +1,39 @@
+using CafremaApp.Core.Enums;
+using CafremaApp.Core.ValueObjects;
+
+namespace CafremaApp.Application.DTOs;
+
+public class InventoryDTO
+{
+    
+    public Guid Id { get; set; }
+    public string Type { get; set; }
+    public DateOnly InstallationDate { get; set; }
+    public Condition Condition { get; set; }
+    public bool NeedsRepair { get; set; }
+    public CommentInfoDTO Comment { get; set; }
+
+    public InventoryDTO(string type, Condition condition)
+    {
+        Type = type;
+        Condition = condition;
+        NeedsRepair = false;
+    }
+    
+}
+
+public class ApplianceDTO : InventoryDTO
+{
+    
+    public string Manufacturer { get; set; }
+    
+    public string Model { get; set; }
+    
+    public ApplianceDTO(string type, Condition condition, 
+        string manufacturer, string model) : base(type, condition)
+    {
+        Manufacturer = manufacturer;
+        Model = model;
+    }
+
+}
