c0e16068ce2f68d83a4d4c8b79fa7b213448819f
Mads D <madsdueholm@live.dk>
2025-11-19 11:50:06 +0100

    Mindre refaktorering

--- CafremaApp.Infrastructure/Repositories/ApplianceRepository.cs
+++ CafremaApp.Infrastructure/Repositories/ApplianceRepository.cs
@@ -52,4 +52,9 @@ public class ApplianceRepository :  IGenericRepository<Appliance>
 
         return entity;
     }
+
+    public async Task SaveChangesAsync()
+    {
+        await _dbContext.SaveChangesAsync();
+    }
 }
